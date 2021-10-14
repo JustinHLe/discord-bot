e4d70941fda48557163e90e2da17246745622461
Justin Le <jle043@ucr.edu>
2021-10-14 16:50:02 -0700

    Delete responses.py

--- responses.py
+++
@@ -1,4 +0,0 @@
-response = ['its over', 'its over dude', 'its beyond over', 'im blackpilled bro', 
-'frustration and animosity', 'tfw no gf', 'no e-girlfriend', 'i hate my life bro', 
-'im too blackpilled to do anything', 'ngmi', 'over before it began', 
-'blackpilled type of night']
